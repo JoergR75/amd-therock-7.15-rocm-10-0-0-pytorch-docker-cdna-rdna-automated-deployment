80383660a634385fc58425af903422bc6a785cbc
Jörg Roskowetz <141847198+JoergR75@users.noreply.github.com>
2026-09-10 15:31:48 +0200

    Update script_module_TheRock_7.15_ROCm_10.0.0_Ubuntu_24.04-26.04_pytorch.sh

--- script_module_TheRock_7.15_ROCm_10.0.0_Ubuntu_24.04-26.04_pytorch.sh
+++ script_module_TheRock_7.15_ROCm_10.0.0_Ubuntu_24.04-26.04_pytorch.sh
@@ -28,7 +28,7 @@ exec > >(tee -a "$LOGFILE") 2>&1
 # ---------------------------------------------------------------------------------------------------------------
 # ROCm Platform:         ROCm 10.0.0 / TheRock 7.15
 # ROCm Release Notes:    https://rocm.docs.amd.com/en/docs-10.0.0/about/release-notes.html
-# amdgpu-dkms:           31.50.0
+# amdgpu:                31.50.0
 #
 # PyTorch:               2.15.0.dev20260907+rocm10.0
 # Transformers:          5.16.1

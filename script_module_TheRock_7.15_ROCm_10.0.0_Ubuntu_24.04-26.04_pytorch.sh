74e0b6619fcadd19e7a7813abdedc5eef6a55c9a
Jörg Roskowetz <141847198+JoergR75@users.noreply.github.com>
2026-09-22 10:10:34 +0200

    Update script_module_TheRock_7.15_ROCm_10.0.0_Ubuntu_24.04-26.04_pytorch.sh

--- script_module_TheRock_7.15_ROCm_10.0.0_Ubuntu_24.04-26.04_pytorch.sh
+++ script_module_TheRock_7.15_ROCm_10.0.0_Ubuntu_24.04-26.04_pytorch.sh
@@ -290,7 +290,7 @@ install_resolute() {
     print '\n 📦 Installing ROCm 10.0.0 / TheRock 7.15 complete Core SDK including runtimes, compilers, development tools, and dependencies...\n'
 
     # Installing complete Core SDK including runtimes, compilers, development tools, and dependencies
-    sudo amdgpu-install --usecase=graphics --gfxversion=all
+    sudo amdgpu-install --usecase=graphics --gfxversion=gfx1201
 
     # Download and install GPG key
     sudo mkdir --parents --mode=0755 /etc/apt/keyrings
